5e7f102e8d1df2cb44ef1e400cb6c6012131e963
Nguyễn Đức Long <duclong@tgm.vn>
2019-11-09 15:30:13 +0700

    5 - Generate a prime number given a lower bound and an upper bound

--- test/math/prime.cpp
+++ test/math/prime.cpp
@@ -4,6 +4,24 @@
 
 using namespace RSA::Math::Prime;
 
+TEST_CASE("const long RSA::Math::Prime::first100[]", "[math]")
+{
+    REQUIRE(sizeof(first100) / sizeof(*first100) == 100);
+
+    for (int i = 0; i < 100; i++)
+    {
+        long n = first100[i];
+
+        SECTION("when n is " + std::to_string(n))
+        {
+            for (int j = 2; j <= SqrRoot(n); j++)
+            {
+                REQUIRE(n % j != 0);
+            }
+        }
+    }
+}
+
 TEST_CASE("bool compositenessWitness(const ZZ &a, const ZZ &n)", "[math]")
 {
     REQUIRE(compositenessWitness(ZZ(0), ZZ(3)) == false);
@@ -26,6 +44,62 @@ TEST_CASE("bool compositenessWitness(const ZZ &a, const ZZ &n)", "[math]")
 
                 REQUIRE(compositenessWitness(j, n) == false);
             }
+        }
+    }
+}
+
+TEST_CASE("ZZ RSA::Math::Prime::Generate(const ZZ &lowerBound, const ZZ &upperBound)", "[math]")
+{
+    ZZ lowerBound, upperBound, n;
+
+    SECTION("generate prime number from 2^2013 to 2^1024")
+    {
+        lowerBound = ZZ(1) << 1023;
+        upperBound = ZZ(1) << 1024;
+
+        for (int i = 0; i < 10; i++)
+        {
+            n = Generate(lowerBound, upperBound);
+
+            REQUIRE(n >= lowerBound);
+            REQUIRE(n < upperBound);
+            REQUIRE(NumBits(n) == 1024);
+            REQUIRE(ProbPrime(n) == true);
+            REQUIRE(Test(n) == true);
+        }
+    }
+
+    SECTION("generate prime number from 2^2013.5 to 2^1024")
+    {
+        lowerBound = ZZ(3) << 1022;
+        upperBound = ZZ(1) << 1024;
+
+        for (int i = 0; i < 10; i++)
+        {
+            ZZ p = Generate(lowerBound, upperBound);
+            ZZ q = Generate(lowerBound, upperBound);
+            n = p * q;
+
+            REQUIRE(NumBits(n) == 2048);
+        }
+    }
+}
+
+TEST_CASE("bool RSA::Math::Prime::Test(const ZZ &n)", "[math]")
+{
+    REQUIRE(Test(ZZ(0)) == false);
+    REQUIRE(Test(ZZ(1)) == false);
+
+    PrimeSeq s;
+    s.next();
+
+    for (int i = 0; i < 250; i++)
+    {
+        ZZ n(s.next());
+
+        SECTION("when n is " + std::to_string(conv<long>(n)))
+        {
+            REQUIRE(Test(n) == true);
         }
     }
 }
